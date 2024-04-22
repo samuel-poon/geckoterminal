c03e0570f1446d965392e2cd6e1ac3f577d4a5b8
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2024-04-22 15:37:43 -0400

    Change error handling on JSONDecodeError

--- geckoterminal/client.py
+++ geckoterminal/client.py
@@ -12,9 +12,9 @@ class Client:
         headers['Accept'] = f'application/json;version={self.version}'
         r = requests.request(method=http_method, url=config.BASE_URL + endpoint, params=params, headers=headers, data=data)
         
-        if r.status_code == 200:
+        try:
             return r.json()
-        else:
-            raise Exception(f'Encountered status code {r.status_code}. See response below:\n{r.text}')
+        except requests.exceptions.JSONDecodeError as e:
+            raise requests.exceptions.JSONDecodeError(f'Encountered status code {r.status_code}. See response below:\n{r.text}', e.doc, e.pos)
         
 client = Client(config.VERSION)
