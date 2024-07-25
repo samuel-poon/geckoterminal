eccd6998cd499dd3a9c372639a6b7ab2dbc91a25
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2024-07-24 21:47:11 -0400

    Upgrade to v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='geckoterminal',
-    version='0.0.5',
+    version='0.0.6',
     long_description=long_description,
     long_description_content_type='text/markdown',
     python_requires=">=3.6",
